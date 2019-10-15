a9c5025ddf944cee67ff86e34840489f31b59a6c
Cap Chen <18879890225@163.com>
2019-10-15 18:24:36 +0800

    增加测试代码

--- app/io/controller/chat.js
+++ app/io/controller/chat.js
@@ -4,6 +4,7 @@ module.exports = app => {
   class Controller extends app.Controller {
     async index() {
       const message = this.ctx.args[0];
+      console.log('s')
       console.log('chat :', message + ' : ' + process.pid);
       const say = await this.ctx.service.queryAdminConf.queryLoanFlag();
       this.ctx.socket.emit('res', say);
